fbdb5fef8de6076477ca536ef7cabf4a221ffe5d
CometShock <cometshockcrypto@gmail.com>
2023-12-03 16:17:00 -0600

    cleanup

--- schema-type/target/debug/deps/schema_type-091be7630e27462f.d
+++
@@ -1,5 +0,0 @@
-/home/artemis/mud-rs/schema-type/target/debug/deps/schema_type-091be7630e27462f.rmeta: src/lib.rs
-
-/home/artemis/mud-rs/schema-type/target/debug/deps/schema_type-091be7630e27462f.d: src/lib.rs
-
-src/lib.rs:
--- schema-type/target/debug/deps/schema_type-36514aa850aa1565.d
+++
@@ -1,5 +0,0 @@
-/home/artemis/mud-rs/schema-type/target/debug/deps/schema_type-36514aa850aa1565.rmeta: src/lib.rs
-
-/home/artemis/mud-rs/schema-type/target/debug/deps/schema_type-36514aa850aa1565.d: src/lib.rs
-
-src/lib.rs:
